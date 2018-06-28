69946d0359bc959d9f917cd1d6c3f928089c32b8
Cristofer [HP] <cristofer.dev@gmail.com>
2018-06-28 00:12:38 -0400

    ADD graph examples for dashboard
     Changes to be committed:
    	modified:   src/components/DashboardGraphics/DashboardGraphics.css
    	modified:   src/components/DashboardGraphics/index.jsx

--- src/components/DashboardGraphics/index.jsx
+++ src/components/DashboardGraphics/index.jsx
@@ -10,14 +10,14 @@ class DashboardGraphics extends Component {
 
   componentDidMount() {
     console.log("DidMount");
-    var ctx = document.getElementById("myChart").getContext('2d');
+    var ctx = document.getElementById("myChart").getContext("2d");
     var myChart = new Chart(ctx, {
       type: "bar",
       data: {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [
           {
-            label: "# of Votes",
+            label: "",
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
               "rgba(255, 99, 132, 0.1)",
@@ -40,11 +40,11 @@ class DashboardGraphics extends Component {
         ]
       },
       options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'Chart.js Bar Chart'
+        legend: {
+          display: false
         },
+        responsive: true,
+        maintainAspectRatio: false,
         scales: {
           yAxes: [
             {
@@ -52,7 +52,123 @@ class DashboardGraphics extends Component {
                 beginAtZero: true
               },
               gridLines: {
-                display: false,
+                display: false
+              }
+            }
+          ]
+        }
+      }
+    });
+    var ctx = document.getElementById("myChart2").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: ["rgba(255, 99, 132, 0.1)"],
+            borderColor: ["rgba(255,99,132,1)"],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: false
+              }
+            }
+          ]
+        }
+      }
+    });
+    var ctx = document.getElementById("myChart3").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.1)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        cutoutPercentage: 40
+      }
+    });
+    var ctx = document.getElementById("myChart4").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["Dia 1", "Dia 2", "Dia 3", "Dia 4", "Dia 5", "Dia 6"],
+        datasets: [
+          {
+            label: "Valor obtenido",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: ["rgba(255, 99, 132, 0.1)"],
+            borderColor: ["rgba(255,99,132,1)"],
+            borderWidth: 1
+          },
+          {
+            label: "Valor proyectado",
+            fill: false,
+            data: [10, 8, 3, 20, 18, 2],
+            backgroundColor: ["rgba(75, 192, 192, 0.2)"],
+            borderColor: ["rgba(75, 192, 192, 1)"],
+            borderWidth: 2,
+            borderDash: [5,5]
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: false
               }
             }
           ]
@@ -65,14 +181,30 @@ class DashboardGraphics extends Component {
     return (
       <div className={css.container}>
         <div className={`${css.g2} ${css.g}`}>
-          <div className={`chart-container`}>
+          Grafico 1
+          <div className={css.graphcontainer}>
             <canvas id="myChart" />
           </div>
         </div>
-        <div className={`${css.g1} ${css.g}`}>Grafico</div>
-        <div className={`${css.g1} ${css.g}`}>Grafico</div>
+        <div className={`${css.g1} ${css.g}`}>
+          Grafico 2
+          <div className={css.graphcontainer}>
+            <canvas id="myChart2" />
+          </div>
+        </div>
+        <div className={`${css.g1} ${css.g}`}>
+          Grafico 3
+          <div className={css.graphcontainer}>
+            <canvas id="myChart3" />
+          </div>
+        </div>
 
-        <div className={`${css.g2} ${css.g}`}>Grafico</div>
+        <div className={`${css.g2} ${css.g}`}>
+          Grafico 4
+          <div className={css.graphcontainer}>
+            <canvas id="myChart4" />
+          </div>
+        </div>
         <div className={`${css.g1} ${css.g}`}>Grafico</div>
         <div className={`${css.g1} ${css.g}`}>Grafico</div>
 
